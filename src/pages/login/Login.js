24b3fdf722dabbb93688ff345ed7c33dcd9e52ac
Taiwo Sodiq <oyeniyi4real@gmail.com>
2023-01-09 11:12:27 +0100

    working on the login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,31 @@
 // static files
+import loginImg from '../../img/login.svg'
 const Login = () => {
   return (
-    <div className="login">Login</div>
+    <div className="login">
+      <div className="logo">
+        <img src="" alt="logo" />
+      </div>
+      <div className="login-img">
+        <img src={loginImg} alt="login" />
+      </div>
+
+      <form>
+        <h1>Welcome</h1>
+        <p>Enter details to login</p>
+        
+        <label>
+          <input type="text" placeholder="name" />
+        </label>
+
+        <label>
+          <input type="password" placeholder="pasword" />
+          <span>Show</span>
+        </label>
+
+        <button></button>
+      </form>
+    </div>
   );
 }
  
