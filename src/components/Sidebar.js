52bfa4d6d16460eac16749537cad26aeaa05f1c9
Taiwo Sodiq <oyeniyi4real@gmail.com>
2023-01-11 11:09:58 +0100

    working on the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,21 @@
+// static files
+import briefcase from "../icon/briefcase.svg"
+import caretdown from "../icon/caretdown.svg"
+import home from "../icon/home.svg"
+
 const Sidebar = () => {
     return (
-      <div className="sidebar">Sidebar</div>
+      <div className="sidebar">
+        <h5 className="display-f align-center">
+          <img className="mr-1" src={briefcase} alt="briefcase icon" />
+          <div className="font-sm text-gray">Switch Organization</div>
+          <img className="ml-1" src={caretdown} alt="caret down icon" />
+        </h5>
+        <h5 className="text-gray-light-3">
+          <img src={home} alt="dashboard icon" />
+          <div className="font-sm text-gray-light-3">Dashboard</div>
+        </h5>
+      </div>
     );
 }
  
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,13 @@
 import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar";
 
 const Dashboard = () => {
     return (
       <div className="dashboard">
         <Navbar />
+        <div>
+          <Sidebar />
+        </div>
       </div>
     );
 }
