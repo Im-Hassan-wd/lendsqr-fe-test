c7efe845ac6c231d85f81fdde8f0fdb567933fa3
Taiwo Sodiq <oyeniyi4real@gmail.com>
2023-01-11 10:48:38 +0100

    sidebar component and the card scss file component

---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+const Card = () => {
+    return (
+      <div className="row">
+        <div className="col-12-xs col-5-sm col-3-xl">
+          <div className="card">
+            <div className="card-img"></div>
+            <div className="card-body">Users</div>
+            <div className="card-h">2,453</div>
+          </div>
+        </div>
+        <div className="col-12-xs col-5-sm col-3-xl">
+          <div className="card">
+            <div className="card-img"></div>
+            <div className="card-body">Active Users</div>
+            <div className="card-h">2,453</div>
+          </div>
+        </div>
+        <div className="col-12-xs col-5-sm col-3-xl">
+          <div className="card">
+            <div className="card-img"></div>
+            <div className="card-body">User with loans</div>
+            <div className="card-h">12,453</div>
+          </div>
+        </div>
+        <div className="col-12-xs col-5-sm col-3-xl">
+          <div className="card">
+            <div className="card-img"></div>
+            <div className="card-body">User with savings</div>
+            <div className="card-h">102,453</div>
+          </div>
+        </div>
+      </div>
+    );
+}
+ 
+export default Card;
