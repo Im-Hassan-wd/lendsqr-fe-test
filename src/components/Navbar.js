a981e849fdfd4321ff297733c096fc64b73a9c7b
Taiwo Sodiq <oyeniyi4real@gmail.com>
2023-01-11 09:13:34 +0100

    working on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 // static files
 import logo from "../img/logo.svg"
+import notification from "../icon/notification.svg"
+import dropdown from "../icon/dropdown.svg"
+import avatar from "../img/avatar.png"
 import "./Navbar.scss"
 
 const Navbar = () => {
@@ -7,16 +10,19 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="container">
           <h1 className='site-title logo'><img src={logo} alt="site logo" /></h1>
-          <div className="display-f">
-            <input type="text" className="form-input" />
+          <div className="display-f align-center">
+            <input type="text" className="form-input form-input-half" />
             <button className='btn-blue border-half-radius text-white'>search</button>
           </div>
-          <div>
-            <a className="text-primary font-sm" href="#">Docs</a>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+          <ul className="display-f align-center">
+            <li className="ml-1 text-md text-hover-primary"><a className="text-primary" href="#">Docs</a></li>
+            <li className="ml-1 text-md text-hover-primary"><img src={notification} alt="notification"/></li>
+            <li className="ml-1 text-md text-hover-primary display-f">
+                <img src={avatar} alt="avatar" />
+                <p className="text-primary">Adedeji</p>
+                <img src={dropdown} alt="drop down" />
+            </li>
+          </ul>
         </div>
       </nav>
     );
