7296d65c7f85ecc760f25ff8e71cd97ad7a068a5
Taiwo Sodiq <alhassansahih@gmail.com>
2023-01-13 22:13:09 +0100

    responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 // static files
 import logo from "../img/logo.svg"
@@ -8,28 +9,32 @@ import avatar from "../img/avatar.png"
 import "./Navbar.scss"
 
 const Navbar = () => {
-    return (
-      <nav className="navbar">
+  const [showOption, setShowOption] = useState('')
+
+  return (
+    <nav className="navbar">
+      <div className="container">
         <div className="container">
-          <div className="container">
-            <Link to="/" className='site-title logo'><img src={logo} alt="site logo" /></Link>
-            <div className="display-f align-center">
-                <input type="text" placeholder="search for anything" className="form-input form-input-half m-0 ml-4 text-primary p-1"/>
-                <button className='btn-blue border-half-radius text-white p-2'><img src={search} alt="search icon" /></button>
-            </div>
+          <Link to="/" className='site-title logo'><img src={logo} alt="site logo" /></Link>
+          <div className="display-f align-center">
+            <input type="text" placeholder="search for anything" className="form-input form-input-half m-0 ml-4 text-primary p-1"/>
+            <button className='btn-blue border-half-radius text-white p-2'><img src={search} alt="search icon" /></button>
           </div>
-          <ul className="display-f align-center">
-            <li className="ml-2 text-md text-hover-primary"><a className="text-primary" href="#">Docs</a></li>
-            <li className="ml-3 text-md text-hover-primary"><img src={notification} alt="notification"/></li>
-            <li className="text-md text-hover-primary display-f align-center">
-                <img className="ml-1 br-full avatar" src={avatar} alt="avatar" />
-                <p className="text-primary ml-1">Adedeji</p>
-                <img className="ml-1 mr-1" src={dropdown} alt="drop down" />
-            </li>
-          </ul>
         </div>
-      </nav>
-    );
+        <ul className="display-f align-center">
+          <li className="ml-2 text-md text-hover-primary"><a className="text-primary" href="#">Docs</a></li>
+          <li className="ml-3 text-md text-hover-primary"><img className="notification" src={notification} alt="notification"/></li>
+          <li className="text-md text-hover-primary display-f align-center">
+            <div className="display-f align-center active">
+              <img className="ml-1 br-full avatar" src={avatar} alt="avatar" />
+              <p className="text-primary ml-1">Adedeji</p>
+            </div>
+            <button ><img src={dropdown} alt="drop down" /></button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
  
 export default Navbar;
