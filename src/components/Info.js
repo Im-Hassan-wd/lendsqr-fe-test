3024b18ea35af565c8e293c446ad65c3b1d16d81
Taiwo Sodiq <alhassansahih@gmail.com>
2023-01-13 12:41:08 +0100

    social component & guarantor

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,13 @@
 import Education from "./Education";
 import PersonalInfo from "./PersonalInfo";
+import Social from "./Social";
 
 const Info = ({ user }) => {
     return (
       <div className="info bg-white mt-3 p-2">
         <PersonalInfo user={user} />
         <Education user={user} />
+        <Social user={user} />
       </div>
     );
 }
