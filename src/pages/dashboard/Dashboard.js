93eeae49100f0939abbdc9b7454d403c9f50ba42
Taiwo Sodiq <alhassansahih@gmail.com>
2023-01-13 18:51:27 +0100

    outputting the error message

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -16,7 +16,7 @@ const Dashboard = ({ user, isPending, error }) => {
             <h1 className="font-lg mt-2 mb-2 text-primary">Dashboard</h1>
             <Card />
             {isPending && <div className="loading"><img src={loading} alt="loading" /></div>}
-            {error && <div className="error mt-4 text-primary font-lg bg-white p-2">{error}, but don't fret-<p className="text-white bg-primary p-1 br-md display-i-b mt-1" onClick={() => window.location.reload()}> let's give it another shot</p></div>}
+            {error && <div className="error mt-2 text-primary font-lg bg-white p-2">{error}, but don't fret-<p className="text-white bg-primary p-1 br-md display-i-b mt-1" onClick={() => window.location.reload()}> let's give it another shot</p></div>}
             {user && <Table user={user.slice(0, 10)} isPending={isPending} />}
           </div>
         </div>
--- src/pages/users/User.js
+++ src/pages/users/User.js
@@ -17,7 +17,7 @@ const User = ({ user, isPending, error, end, start, setStart, setEnd }) => {
           <div className="container">
             <h1 className="font-lg mt-2 mb-2 text-primary">Users</h1>
             <Card />
-            {error && <div className="error mt-4 text-primary font-lg bg-white p-2">{error}, but don't fret-<p className="text-white bg-primary p-1 br-md display-i-b mt-1" onClick={() => window.location.reload()}> let's give it another shot</p></div>}
+            {error && <div className="error mt-2 text-primary font-lg bg-white p-2">{error}, but don't fret-<p className="text-white bg-primary p-1 br-md display-i-b mt-1" onClick={() => window.location.reload()}> let's give it another shot</p></div>}
             {isPending && <div className="loading"><img src={loading} alt="loading" /></div>}
             <div className="wrapper">
               {user && <Table 
