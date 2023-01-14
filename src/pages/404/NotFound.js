d80e30440138b79652c2c2391e478884aa81c1d0
Taiwo Sodiq <alhassansahih@gmail.com>
2023-01-14 10:15:02 +0100

    404 page

--- src/pages/404/NotFound.js
+++ src/pages/404/NotFound.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import { Helmet } from 'react-helmet-async';
+import Navbar from "../../components/Navbar";
 
 export default function NotFound () {
   return (
-    <div className="container">
+    <div className="bg-white">
       <Helmet>
         <title>404</title>
         <meta name="description" content="Not found" />
@@ -11,9 +12,10 @@ export default function NotFound () {
       </Helmet>
       <div className="not-found">
         <div className="bg-primary">
-          <h1 className="font-xxl text-white">404</h1>
-          <h2 className="font-xl text-white">This is not the web page your are looking for</h2>
-          <Link className="bg-white font-lg text-primary p-2" to="/search">Back to safety</Link>
+          <Navbar />
+          <h1 className="font-xxl text-white pl-5">404</h1>
+          <h2 className="font-xl text-white pl-5 pr-5">This is not the web page your are looking for</h2>
+          <Link className="bg-white font-lg text-primary p-2" to="/">Back to safety</Link>
         </div>
       </div>
     </div>
